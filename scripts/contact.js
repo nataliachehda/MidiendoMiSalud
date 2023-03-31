347654d374c0efa60664f6a012a77cb382cde322
nataliachehda <116130811+nataliachehda@users.noreply.github.com>
2023-03-31 16:43:49 -0300

    faltaba cambiar  el endpoint en el javascript

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,32 +1,34 @@
-/*llevé la info del formulario al LocalStorage al no contar con servidor */
-
 $(document).ready(function() {
-    const form = document.querySelector('#formContacto');
-    form.addEventListener('submit', function(event) {
-      event.preventDefault();
-  
-      const nombre = document.querySelector('#nombre').value;
-      const email = document.querySelector('#email').value;
-      const mensaje = document.querySelector('#mensaje').value;
-  
-      const datos = {
-        nombre,
-        email,
-        mensaje
-      };
-  
-      const datosJSON = JSON.stringify(datos);
-  
-      localStorage.setItem('formularioContacto', datosJSON);
-  
-      form.reset();
-  
-      swal.fire({
-        position: 'top-end',
-        icon: 'success',
-        title: 'El mensaje ha sido enviado',
-        showConfirmButton: false,
-        timer: 1500
-      });
-    });
+  const form = document.querySelector('#formContacto');
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const nombre = document.querySelector('#nombre').value;
+    const email = document.querySelector('#email').value;
+    const mensaje = document.querySelector('#mensaje').value;
+
+    // Enviar los datos a Getform
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://getform.io/f/<tu-endpoint>');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        // Si la solicitud se envió correctamente, mostrar un mensaje de éxito
+        swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: 'El mensaje ha sido enviado',
+          showConfirmButton: false,
+          timer: 1500
+        });
+
+        form.reset();
+      }
+    };
+    xhr.send(JSON.stringify({
+      nombre,
+      email,
+      mensaje
+    }));
   });
+});
